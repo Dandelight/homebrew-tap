7af4b0c3aea07673063a0a7995d65d9eea6633b2
Raymond Guo <raymond@qingnan.tech>
2025-09-09 11:54:29 +0800

    update: `cmake@3` to `v3.31.8`

--- Formula/cmake@3.rb
+++ Formula/cmake@3.rb
@@ -1,10 +1,10 @@
 class CmakeAT3 < Formula
   desc "Cross-platform make"
   homepage "https://www.cmake.org/"
-  url "https://github.com/Kitware/CMake/releases/download/v3.31.6/cmake-3.31.6.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/cmake-3.31.6.tar.gz"
-  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.31.6.tar.gz"
-  sha256 "653427f0f5014750aafff22727fb2aa60c6c732ca91808cfb78ce22ddd9e55f0"
+  url "https://github.com/Kitware/CMake/releases/download/v3.31.8/cmake-3.31.8.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.31.8.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.31.8.tar.gz"
+  sha256 "e3cde3ca83dc2d3212105326b8f1b565116be808394384007e7ef1c253af6caa"
   license "BSD-3-Clause"
   head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
 
@@ -21,11 +21,6 @@ class CmakeAT3 < Formula
   on_linux do
     depends_on "openssl@3"
   end
-
-  # Prevent the formula from breaking on version/revision bumps.
-  # Check if possible to remove in 3.32.0
-  # https://gitlab.kitware.com/cmake/cmake/-/merge_requests/9978
-  patch :DATA
 
   # The completions were removed because of problems with system bash
 
@@ -74,26 +69,3 @@ class CmakeAT3 < Formula
     refute_path_exists man
   end
 end
-
-__END__
-diff --git a/Source/cmSystemTools.cxx b/Source/cmSystemTools.cxx
-index 5ad0439c..161257cf 100644
---- a/Source/cmSystemTools.cxx
-+++ b/Source/cmSystemTools.cxx
-@@ -2551,7 +2551,7 @@ void cmSystemTools::FindCMakeResources(const char* argv0)
-     _NSGetExecutablePath(exe_path, &exe_path_size);
-   }
-   exe_dir =
--    cmSystemTools::GetFilenamePath(cmSystemTools::GetRealPath(exe_path));
-+    cmSystemTools::GetFilenamePath(exe_path);
-   if (exe_path != exe_path_local) {
-     free(exe_path);
-   }
-@@ -2572,7 +2572,6 @@ void cmSystemTools::FindCMakeResources(const char* argv0)
-   std::string exe;
-   if (cmSystemTools::FindProgramPath(argv0, exe, errorMsg)) {
-     // remove symlinks
--    exe = cmSystemTools::GetRealPath(exe);
-     exe_dir = cmSystemTools::GetFilenamePath(exe);
-   } else {
-     // ???
