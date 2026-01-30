4e8e9927feb8fe145ac17889e284501b1744e8f5
Ruiming Guo <raymond@qingnan.tech>
2026-01-30 11:00:26 +0800

    update: `coscli` to `1.0.7`

--- Formula/coscli.rb
+++ Formula/coscli.rb
@@ -1,13 +1,13 @@
 class Coscli < Formula
   desc "Tencent Cloud COS Command-Line Interface"
   homepage "https://github.com/tencentyun/coscli"
-  url "https://github.com/tencentyun/coscli/releases/download/v1.0.6/coscli-v1.0.6-darwin-arm64"
+  url "https://github.com/tencentyun/coscli/releases/download/v1.0.7/coscli-v1.0.7-darwin-arm64"
   # mirror "https://cosbrowser.cloud.tencent.com/software/coscli/coscli-darwin-arm64"
-  version "v1.0.6"
-  sha256 "9253e043985b51219112400a37b39e618ab5c358cf5777193829aa7b9d9ff5f6"
+  version "v1.0.7"
+  sha256 "66b910ec6cc9d182c6b6625b3c6c7c9336fff1967198e5ca9b5581e6b6b8c0ac"
 
   def install
-    bin.install "coscli-v1.0.6-darwin-arm64" => "coscli"
+    bin.install "coscli-v1.0.7-darwin-arm64" => "coscli"
     chmod 0755, bin/"coscli"
   end
 
